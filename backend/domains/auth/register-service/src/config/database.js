424a90df3cff853dbed21b2c53e223c7dfc974b6
MateoRivadeneira14 <mateoriva98@gmail.com>
2025-07-05 08:19:03 -0500

    Ahora si funka

--- backend/domains/auth/register-service/src/config/database.js
+++ backend/domains/auth/register-service/src/config/database.js
@@ -2,16 +2,6 @@ const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DB_URL, {
-<<<<<<< HEAD
-    dialect: 'postgres',
-    logging: false,
-    dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false
-        }
-    }
-=======
   dialect: 'postgres',
   logging: false,
   dialectOptions: {
@@ -20,7 +10,6 @@ const sequelize = new Sequelize(process.env.DB_URL, {
       rejectUnauthorized: false // let conection without CA validate
     }
   }
->>>>>>> 8b13fef8a6c49356c4d44e0a6179de14531a9888
 });
 
 module.exports = { sequelize };
