69169a949a7ca506d71099d9741e13743e79ec53
MateoRivadeneira14 <mateoriva98@gmail.com>
2025-07-05 07:59:25 -0500

    Test DB

--- backend/domains/auth/verify-code-service/src/config/database.js
+++ backend/domains/auth/verify-code-service/src/config/database.js
@@ -1,7 +1,5 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
-console.log("DB_URL desde .env:", process.env.DB_URL);
-
 
 const sequelize = new Sequelize(process.env.DB_URL, {
   dialect: 'postgres',
@@ -9,11 +7,7 @@ const sequelize = new Sequelize(process.env.DB_URL, {
   dialectOptions: {
     ssl: {
       require: true,
-<<<<<<< HEAD
-      rejectUnauthorized: false
-=======
-      rejectUnauthorized: false // permite conexión sin validar CA
->>>>>>> 8b13fef8a6c49356c4d44e0a6179de14531a9888
+      rejectUnauthorized: false // let conection without CA validate
     }
   }
 });
